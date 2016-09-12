4be2a70d7e4ba2615ff88453529e2ee3641fb533
CoffeeAndCream <seanjhulse@gmail.com>
2016-09-12 15:45:53 -0500

    1.2 added

--- 1.2/1.2/Source.cpp
+++ 1.2/1.2/Source.cpp
@@ -1,7 +1,33 @@
+//Cracking the Coding Interview
+//Chapter 1
+//Interview Question 1.2
+
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main(){
+void reverse(char*);
 
+int main(){
+	char *str = "It's getting late."; 
+	reverse(str);
+
+	system("pause");
+	return 0;
+}
+
+void reverse(char *str){
+	//reverses a null-terminated string
+	string s(str);
+	vector<char> reverse;
+	for (auto i : s){
+		reverse.push_back(i);
+	}
+
+	for (auto character = reverse.crbegin(); character != reverse.crend(); character++){
+		cout << *character;
+	}
+	cout << endl;
 }
