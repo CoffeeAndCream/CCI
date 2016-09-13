6bc29f2d4ba3a27b85ef0972c5b6ce348821cea2
CoffeeAndCream <seanjhulse@gmail.com>
2016-09-13 18:17:20 -0500

    1.5 added

---
+++ 1.5/1.5.cpp
@@ -0,0 +1,49 @@
+/*Cracking the Coding Interview
+Chapter 1
+Interview Question 1.5
+Write a method to compress strings. Return the original string.
+Example:
+	Input: "aabbcccc"
+	Compression: "a2b2c4"
+    Output: "aabbcccc"
+*/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+string compressionStr(string);
+
+int main(){
+    string a = "BEAuuuutiful";
+    compressionStr(a);
+    
+    return 0;
+}
+
+string compressionStr(string a){
+    stringstream sBuff;    
+    char last = a.at(0);
+    int count = 1;
+
+    for(int i = 1; i < a.length(); i++){
+        if(a.at(i) == last){
+            count++;
+        }
+        else{
+            sBuff << last;
+            sBuff << count;
+            last = a.at(i);
+            count = 1;
+        }
+    }
+
+    sBuff << last;
+    sBuff << count;
+    string s = sBuff.str();
+    cout << s << endl;
+    
+    return s;
+}
